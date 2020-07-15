d88758e2a7e3f75cd53babdfeec27ce79a08c257
Yurie <cutecupcakeandcute@gmail.com>
2020-07-15 14:28:58 +0900

    add handler to root path to greet 'hello world' on port 8081

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
+	"log"
+	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	greeting := "Hello World!"
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(greeting)
+}
+
 func main() {
-	fmt.Println("Hello World")
 	router := mux.NewRouter()
+
+	router.HandleFunc("/", helloWorld).Methods("GET")
+	log.Println("Listening...")
+	log.Fatal(http.ListenAndServe(":8081", router))
 }
