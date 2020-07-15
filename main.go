97d649d265e03d2cf76cd993f27f88f6fdc8129c
Yurie <cutecupcakeandcute@gmail.com>
2020-07-15 21:01:18 +0900

    add function to read env variables from .env

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func helloWorld(response http.ResponseWriter, request *http.Request) {
@@ -14,10 +16,21 @@ func helloWorld(response http.ResponseWriter, request *http.Request) {
 	json.NewEncoder(response).Encode(greeting)
 }
 
+func loadEnvFileAndReturnEnvVarValueByKey(key string) string {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+	return os.Getenv(key)
+}
+
 func main() {
+	testEnv := loadEnvFileAndReturnEnvVarValueByKey("TEST")
+
 	router := mux.NewRouter()
 
 	router.HandleFunc("/", helloWorld).Methods("GET")
 	log.Println("Listening...")
 	log.Fatal(http.ListenAndServe(":8081", router))
+
 }
